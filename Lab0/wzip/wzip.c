d09234779618a05ee0124499ee1a102eca2fc7ee
Alexander Kim <tuf49831@temple.edu>
2020-01-22 14:59:46 -0500

    Starting wzip

--- Lab0/wzip/wzip.c
+++ Lab0/wzip/wzip.c
@@ -7,8 +7,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, const char *argv[]) {
+int main(int argc, const char * argv[]) {
     
+    if(argc == 1) {
+        printf("wzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+    
+    if(argc >= 2) {
+        FILE *fp1 = fopen(argv[1], "r");
+        
+        if(fp1 == NULL) {
+            printf("wzip: cannot open file\n");
+            perror("Error");
+            exit(1);
+        }
+        
+        FILE *fp2 = fopen(argv[2], "w");
+        
+        if(fp2 == NULL) {
+            printf("wzip: cannot open file\n");
+            perror("Error");
+            exit(1);
+        }
+        
+        char c = fgetc(fp1);
+        while(c != EOF) {
+            
+            
+        }
+        
+        fclose(fp1);
+        fclose(fp2);
+        
+        exit(0);
+    }
     
     
     
